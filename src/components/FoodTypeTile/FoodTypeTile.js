3912d361fca424dc745f6873d28bae8d827a3b95
Thulitha Nipun <nthulitha@gmail.com>
2023-03-03 00:34:41 +0530

    Tile Create

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -6,9 +6,11 @@ import breakfast_image from '../../assets/breakfast.png'
 const FoodTypeTile = () => {
   return (
     <div className={classes.outer}>
-        <img src={breakfast_image} alt=''/>
+        <img className={classes.smallIcon} src={breakfast_image} alt=''/>
+        <div className={classes.titleHolder}>
         <span className={classes.mainTitle} >Breakfast</span>
-        <span>13 Items</span>
+        <span className={classes.subTitle}  >13 Items</span>
+        </div>
     </div>
   )
 }
