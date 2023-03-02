217ebc93180b6ec4dc628b24dc3c34dddbdde373
Thulitha Nipun <nthulitha@gmail.com>
2023-03-02 23:14:50 +0530

    Image Added

---
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import classes from './FoodTypeTile.module.css';
+import breakfast_image from '../../assets/breakfast.png'
+
+
+const FoodTypeTile = () => {
+  return (
+    <div>
+        <img src={breakfast_image} alt=''/>
+    </div>
+  )
+}
+
+export default FoodTypeTile
