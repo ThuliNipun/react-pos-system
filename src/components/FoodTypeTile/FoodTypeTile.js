71ef3f49b4c2ebd4076216d5026db70d24d88dff
Thulitha Nipun <nthulitha@gmail.com>
2023-03-03 00:01:11 +0530

    Title styling

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -1,12 +1,14 @@
 import React from 'react'
-// import classes from './FoodTypeTile.module.css';
+import classes from './FoodTypeTile.module.css';
 import breakfast_image from '../../assets/breakfast.png'
 
 
 const FoodTypeTile = () => {
   return (
-    <div>
+    <div className={classes.outer}>
         <img src={breakfast_image} alt=''/>
+        <span className={classes.mainTitle} >Breakfast</span>
+        <span>13 Items</span>
     </div>
   )
 }
